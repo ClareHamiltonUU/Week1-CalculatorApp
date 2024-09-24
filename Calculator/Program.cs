a8511433264eef2ec95c51786ac12627aa9ef175
Clare Hamilton <csf23hac>
2024-09-24 15:39:48 +0100

    Initial commit

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ask user to input first number
+Console.WriteLine("Type in the first number followed by the Enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Ask user for second number
+Console.WriteLine("Type the second number followed by the Enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// Perform Calculation
+int result = firstNumber + secondNumber;
+
+// Output the asnwer to console
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+
+
+
